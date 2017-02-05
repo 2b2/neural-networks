39dd63009c3a0207acc37d7cd6a5fa6fb875e2cc
2b2 <erik.fritzsche.ef@gmail.com>
2017-02-05 16:38:02 +0100

    Setup kernels

--- FastFlood/src/de/ef/fastflood/opencl/FastFloodOpenCL.java
+++ FastFlood/src/de/ef/fastflood/opencl/FastFloodOpenCL.java
@@ -29,17 +29,19 @@ public class FastFloodOpenCL
 	private final static long serialVersionUID = 002_000_000L;
 	
 	private final static int
-		NEURON_COUNTS_INDEX = 4, NEURON_OFFSETS_INDEX = 3,
-		INPUTS_INDEX = 2, OUTPUTS_INDEX = 1, WEIGHTS_INDEX = 0;
+		INPUTS_INDEX = 0, OUTPUTS_INDEX = 1, WEIGHTS_INDEX = 2,
+		NEURON_COUNTS_INDEX = 3, NEURON_OFFSETS_INDEX = 4,
+		CURRENT_LAYER_INDEX = 5;
 	
 	
 	
 	private final cl_context context;
 	private final cl_command_queue commandQueue;
 	private final Pointer pointers[];
+	private final int inputByteSize, outputByteSize;
 	private final cl_mem memory[];
 	private final cl_program program;
-	private final cl_kernel kernel;
+	private final cl_kernel calculatdeLayerKernel, trainLayerKernel, randomFloatArrayKernel;
 	
 	
 	@SuppressWarnings("deprecation")
@@ -48,102 +50,89 @@ public class FastFloodOpenCL
 		
 		cl_context_properties contextProperties = new cl_context_properties();
 		contextProperties.addProperty(CL_CONTEXT_PLATFORM, config.platform);
-		this.context = clCreateContext(contextProperties, 1, new cl_device_id[]{config.device}, null, null, null);
+		context = clCreateContext(contextProperties, 1, new cl_device_id[]{config.device}, null, null, null);
 		// using old command queue to support OpenCL 1.2
-		this.commandQueue = clCreateCommandQueue(context, config.device, 0, null);
-		//this.commandQueue = clCreateCommandQueueWithProperties(context, device, null, null);
+		commandQueue = clCreateCommandQueue(context, config.device, 0, null);
+		//commandQueue = clCreateCommandQueueWithProperties(context, device, null, null);
 		
-		this.pointers = new Pointer[3];
-		this.pointers[INPUTS_INDEX] = Pointer.to(this.inputs);
-		this.pointers[OUTPUTS_INDEX] = Pointer.to(this.outputs);
-		this.pointers[WEIGHTS_INDEX] = Pointer.to(this.weights);
+		pointers = new Pointer[2];
+		pointers[INPUTS_INDEX] = Pointer.to(inputs);
+		pointers[OUTPUTS_INDEX] = Pointer.to(outputs);
 		
-		this.memory = new cl_mem[5];
-		this.memory[NEURON_COUNTS_INDEX] = clCreateBuffer(
-			context,
-			CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-			Sizeof.cl_int * this.neuronCounts.length,
-			Pointer.to(this.neuronCounts),
-			null
+		inputByteSize = Sizeof.cl_float * inputs.length;
+		outputByteSize = Sizeof.cl_float * outputs.length;
+		
+		memory = new cl_mem[5];
+		memory[INPUTS_INDEX] = clCreateBuffer(
+			context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, inputByteSize, pointers[INPUTS_INDEX], null
 		);
-		this.memory[NEURON_OFFSETS_INDEX] = clCreateBuffer(
-			context,
-			CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-			Sizeof.cl_int * this.neuronOffsets.length,
-			Pointer.to(this.neuronOffsets),
-			null
+		memory[OUTPUTS_INDEX] = clCreateBuffer(
+			context, CL_MEM_READ_WRITE, outputByteSize, pointers[OUTPUTS_INDEX], null
 		);
-		this.memory[INPUTS_INDEX] = clCreateBuffer(
-			context,
-			CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-			Sizeof.cl_float * this.inputs.length,
-			this.pointers[INPUTS_INDEX],
-			null
+		memory[WEIGHTS_INDEX] = clCreateBuffer(
+			context, CL_MEM_READ_WRITE, Sizeof.cl_float * weightCount, pointers[WEIGHTS_INDEX], null
 		);
-		this.memory[OUTPUTS_INDEX] = clCreateBuffer(
-			context,
-			CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, // or CL_MEM_USE_HOST_PTR or nothing
-			Sizeof.cl_float * this.outputs.length,
-			this.pointers[OUTPUTS_INDEX],
-			null
+		memory[NEURON_COUNTS_INDEX] = clCreateBuffer(
+			context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, Sizeof.cl_int * neuronCounts.length, Pointer.to(neuronCounts), null
 		);
-		this.memory[WEIGHTS_INDEX] = clCreateBuffer(
-			context,
-			CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, // or CL_MEM_USE_HOST_PTR or nothing
-			Sizeof.cl_float * this.weights.length,
-			this.pointers[WEIGHTS_INDEX],
-			null
+		memory[NEURON_OFFSETS_INDEX] = clCreateBuffer(
+			context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, Sizeof.cl_int * neuronOffsets.length, Pointer.to(neuronOffsets), null
 		);
 		
-		this.program = ProgramBuilder.loadAndBuildProgram(context, config.device, "/fast-flood.cl", true);
+		program = ProgramBuilder.loadAndBuildProgram(context, config.device, "/fast-flood.cl", true);
 		
-		this.kernel = clCreateKernel(program, "TODO", null);
-		// set kernel arguments, reverse order
-		for(int i = 0; i < this.memory.length; i++){
-			clSetKernelArg(this.kernel, i, Sizeof.cl_mem, Pointer.to(this.memory[this.memory.length - (i + 1)]));
+		calculatdeLayerKernel = clCreateKernel(program, "calculateLayer", null);
+		trainLayerKernel = clCreateKernel(program, "trainLayer", null);
+		// set kernel arguments
+		for(int i = 0; i < memory.length; i++){
+			clSetKernelArg(calculatdeLayerKernel, i, Sizeof.cl_mem, Pointer.to(memory[i]));
+			clSetKernelArg(trainLayerKernel, i, Sizeof.cl_mem, Pointer.to(memory[i]));
 		}
+		
+		randomFloatArrayKernel = clCreateKernel(program, "randomFloatArray", null);
+		clSetKernelArg(randomFloatArrayKernel, 0, Sizeof.cl_mem, Pointer.to(memory[WEIGHTS_INDEX]));
+		
+		// fill weight random
+		clEnqueueNDRangeKernel(
+			commandQueue, randomFloatArrayKernel, 1, null, new long[]{weightCount}, new long[]{1}, 0, null, null
+		);
+		clFinish(commandQueue);
 	}
 	
 	
 	@Override
 	protected void calculateLayer(int layer) throws IOException{
+		clSetKernelArg(calculatdeLayerKernel, CURRENT_LAYER_INDEX, Sizeof.cl_int, Pointer.to(new int[]{layer}));
 		
+		clEnqueueNDRangeKernel(
+			commandQueue, calculatdeLayerKernel, 1, null, new long[]{neuronCounts[layer]}, new long[]{1}, 0, null, null
+		);
+		clFinish(commandQueue);
 	}
 	
 	
 	@Override
 	protected void trainLayer(int layer) throws IOException{
+		clSetKernelArg(trainLayerKernel, CURRENT_LAYER_INDEX, Sizeof.cl_int, Pointer.to(new int[]{layer}));
 		
+		clEnqueueNDRangeKernel(
+			commandQueue, trainLayerKernel, 1, null, new long[]{neuronCounts[layer]}, new long[]{1}, 0, null, null
+		);
+		clFinish(commandQueue);
 	}
 	
 	
 	@Override
 	protected void writeInputs() throws IOException{
 		clEnqueueWriteBuffer(
-			/* TODO queue */null,
-			memory[INPUTS_INDEX],
-			CL_TRUE,
-			0,
-			outputs.length * Sizeof.cl_float,
-			pointers[INPUTS_INDEX],
-			0,
-			null,
-			null
+			commandQueue, memory[INPUTS_INDEX], CL_TRUE, 0, inputByteSize, pointers[INPUTS_INDEX], 0, null, null
 		);
 	}
 	
 	@Override
 	protected void readOutputs() throws IOException{
 		clEnqueueReadBuffer(
-			/* TODO queue */null,
-			memory[OUTPUTS_INDEX],
-			CL_TRUE,
-			0,
-			outputs.length * Sizeof.cl_float,
-			pointers[OUTPUTS_INDEX],
-			0,
-			null,
-			null
+			commandQueue, memory[OUTPUTS_INDEX], CL_TRUE, 0, outputByteSize, pointers[OUTPUTS_INDEX], 0, null, null
 		);
 	}
 	
@@ -153,7 +142,11 @@ public class FastFloodOpenCL
 		for(int i = 0; i < memory.length; i++){
 			clReleaseMemObject(memory[i]);
 		}
-		clReleaseKernel(kernel);
+		
+		clReleaseKernel(calculatdeLayerKernel);
+		clReleaseKernel(trainLayerKernel);
+		clReleaseKernel(randomFloatArrayKernel);
+		
 		clReleaseProgram(program);
 		clReleaseCommandQueue(commandQueue);
 		clReleaseContext(context);
