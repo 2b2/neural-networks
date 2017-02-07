36b1f8e24cda36326f3775edcad2e7fed8fe20ad
2b2 <erik.fritzsche.ef@gmail.com>
2017-02-07 12:39:07 +0100

    Add auto wrapper

--- NeuralNetworks/src/de/ef/neuralnetworks/NeuralNetworkWrapper.java
+++ NeuralNetworks/src/de/ef/neuralnetworks/NeuralNetworkWrapper.java
@@ -6,7 +6,68 @@ import java.util.function.Function;
 public final class NeuralNetworkWrapper<I, O, IW, OW>
 	implements NeuralNetwork<IW, OW>{
 	
-	private static final long serialVersionUID = 1L;
+	@SuppressWarnings("unchecked")
+	public final static <I, O> NeuralNetwork<I, O> wrap(
+			NeuralNetwork<float[], float[]> network, Class<I> inputClass, Class<O> outputClass){
+		if(inputClass == float[].class){
+			if(outputClass == float[].class)
+				return (NeuralNetwork<I, O>)network;
+			
+			OutputConverter<float[], O> outputWrap = wrapOutput(outputClass);
+			return (NeuralNetwork<I, O>)
+				new NeuralNetworkOutputWrapper<>(network, outputWrap.forward, outputWrap.reverse);
+		}
+		else if(outputClass == float[].class)
+			return (NeuralNetwork<I, O>)new NeuralNetworkInputWrapper<>(network, wrapInput(inputClass));
+		
+		OutputConverter<float[], O> outputWrap = wrapOutput(outputClass);
+		return (NeuralNetwork<I, O>)
+			new NeuralNetworkWrapper<>(network, wrapInput(inputClass), outputWrap.forward, outputWrap.reverse);
+	}
+	
+	private final static <I> Function<I, float[]> wrapInput(Class<I> inputClass){
+		if(inputClass == Float.class) return i -> new float[]{(Float)i};
+		if(inputClass.getSuperclass() == Number.class) return i -> new float[]{((Number)i).floatValue()};
+		throw new NoWrapperFoundException("No input wrapper found.");
+	}
+	
+	@SuppressWarnings("unchecked")
+	private final static <O> OutputConverter<float[], O> wrapOutput(Class<O> outputClass){
+		OutputConverter<?, ?> converter;
+		if(outputClass == Float.class)
+			converter = new OutputConverter<float[], Float>(o -> o[0], o -> new float[]{(Float)o});
+		else if(outputClass == Double.class)
+			converter = new OutputConverter<float[], Double>(o -> Double.valueOf(o[0]), o -> new float[]{o.floatValue()});
+		else throw new NoWrapperFoundException("No output wrapper found.");
+		
+		return (OutputConverter<float[], O>)converter;
+	}
+	
+	
+	private static class OutputConverter<O, OW>{
+		
+		private final Function<O, OW> forward;
+		private final Function<OW, O> reverse;
+		
+		private OutputConverter(Function<O, OW> forward, Function<OW, O> reverse){
+			this.forward= forward;
+			this.reverse = reverse;
+		}
+	}
+	
+	public static class NoWrapperFoundException
+		extends RuntimeException{
+		
+		private final static long serialVersionUID = 1L;
+		
+		private NoWrapperFoundException(String message){
+			super(message);
+		}
+	}
+	
+	
+	
+	private final static long serialVersionUID = 1L;
 	// TODO serial conversion passthru
 	
 	
@@ -17,7 +78,7 @@ public final class NeuralNetworkWrapper<I, O, IW, OW>
 	private final Function<OW, O> reverseOutputConverter;
 	
 	
-	public NeuralNetworkWrapper(NeuralNetwork<I, O> network,
+	private NeuralNetworkWrapper(NeuralNetwork<I, O> network,
 			Function<IW, I> inputConverter, Function<O, OW> outputConverter, Function<OW, O> reverseOutputConverter){
 		this.network = network;
 		
